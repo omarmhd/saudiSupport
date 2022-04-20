1b1b28d43f99fa148cb8e89bbb1a437ca2533a18
omar al haddad <42090545+omarmhd@users.noreply.github.com>
2022-04-21 02:33:57 +0300

    ddd

--- resources/views/layouts/inc/_header.blade.php
+++ resources/views/layouts/inc/_header.blade.php
@@ -53,13 +53,18 @@
 
                 <div class="dropdown nav-item main-header-notification">
                     <a class="new nav-link" href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="header-icon-svgs" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg><span class=" pulse"></span></a>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="header-icon-svgs" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
+                        @if(!auth()->user()->readNotifications())
+                        <span class="pulse"></span>
+                        @endif
+
+                    </a>
                     <div class="dropdown-menu">
                         <div class="menu-header-content bg-primary text-right">
                             <div class="d-flex">
                                 <h6 class="dropdown-title mb-1 tx-15 text-white font-weight-semibold">Notifications</h6>
                             </div>
-                            <p class="dropdown-title-text subtext mb-0 text-white op-6 pb-0 tx-12 ">You have Notifications</p>
+                            <p class="dropdown-title-text subtext mb-0 text-white op-6 pb-0 tx-12 ">You have {{auth()->user()->readNotifications()->count()}} unread Notifications</p>
                         </div>
                         <div class="main-notification-list Notification-scroll" style="overflow: scroll">
 
